71b215b2862a2ac22382625955d0b22889e03047
jclos305 <jclos305@hotmail.com>
2022-03-29 20:38:54 -0400

    added public folder and css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
@@ -8,7 +8,17 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//  Middleware
+// Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: true})); // 
 
+//  application/json Parsing
+// app.use(bodyParser.json());
+app.use(express.json()); // 
+
+// Static-Files
+app.use(express.static('public'));
 
 
 
