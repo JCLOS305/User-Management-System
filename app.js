b8fb819b3c85386cb47ed1927b858d098e2c9129
jclos305 <jclos305@hotmail.com>
2022-03-30 20:03:34 -0400

    connected db through pool connection

--- app.js
+++ app.js
@@ -27,10 +27,20 @@ app.set('view engine', 'hbs');
 
 // Connection Pool
 const pool = mysql.createPool({
+     connectionLimit : 100, 
+
+     host            : process.env.DB_Host,
+     user            : process.env.DB_USER,
+     password        : process.env.DB_PASS,
+     database        : process.env.DB_NAME
 
 });
 
-
+// connect to db
+pool.getConnection((err, connection) => {
+    if (err) throw err; // broken connection
+    console.log('Connected as ID ' + connection.threadId);
+});
 
 
 
