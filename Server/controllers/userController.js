93966100dbcf45e106b00023234c177e0b915daa
jclos305 <jclos305@hotmail.com>
2022-04-03 22:11:29 -0400

    finished

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,89 +1,144 @@
 const mysql = require('mysql');
 
-
-
 // Connection Pool
-const pool = mysql.createPool({
-    connectionLimit : 100,  
-    host            : process.env.DB_HOST,
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASS,
-    database        : process.env.DB_NAME
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
+// View Users
+exports.view = (req, res) => {
+  // User the connection
+  connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
+    // When done with the connection, release it
+    if (!err) {
+      let removedUser = req.query.removed;
+      res.render('home', { rows, removedUser });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
 
+// Find User by Search
+exports.find = (req, res) => {
+  let searchTerm = req.body.search;
+  // User the connection
+  connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
+    if (!err) {
+      res.render('home', { rows });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
 
+exports.form = (req, res) => {
+  res.render('add-user');
+}
 
-// view users
-exports.view = (req,res) => {
+// Add new user
+exports.create = (req, res) => {
+  const { first_name, last_name, email, phone, comments } = req.body;
+  let searchTerm = req.body.search;
 
-// connect to db
-pool.getConnection((err, connection) => {
-   if (err) throw err; // broken connection
-   console.log('Connected as ID ' + connection.threadId);
-
-    // user the connection
-    connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
-     // when done with the connection , release it
-     connection.release();
-
-     if(!err) {
-         res.render('home', { rows });
-        } else {
-            console.log(broken);
-        }
-
-
-        console.log('The data from user table: \n, rows');
-    });
+  // User the connection
+  connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
+    if (!err) {
+      res.render('add-user', { alert: 'User added successfully.' });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
   });
 }
 
 
-// Find User by Searchbar
-exports.find = (req, res) => {
-
-    pool.getConnection((err, connection) => {
-        if (err) throw err; // broken connection
-        console.log('Connected as ID ' + connection.threadId);
-        let searchTerm = req.body.search;
-         // user the connection
-         connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%','%' + searchTerm + '%'], (err, rows) => {
-          // when done with the connection , release it
-          connection.release();
-          if(!err) {
-              res.render('home', { rows });
-             } else {
-                 console.log(err);
-             }
-        console.log('The data from user table: \n', rows);
-         });
-       });
+// Edit user
+exports.edit = (req, res) => {
+  // User the connection
+  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+    if (!err) {
+      res.render('edit-user', { rows });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
 }
 
-exports.form = (req, res) => {
-    res.render('add-user');
-    }
-// add new user 
-exports.create = (req, res) => {
 
-    const {first_name, last_name, email, phone, comments } = req.body;
-    // res.render('add-user');
-    pool.getConnection((err, connection) => {
-        if (err) throw err; // broken connection
-        console.log('Connected as ID ' + connection.threadId);
-        let searchTerm = req.body.search;
-         
-         // user the connection
-         connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?',[first_name,last_name, email, phone, comments],(err, rows) => {
-          // when done with the connection , release it
-          connection.release();
-          if(!err) {
-              res.render('add-user', { alert:'User added successfully.'});
-             } else {
-                 console.log(err);
-             }
+// Update User
+exports.update = (req, res) => {
+  const { first_name, last_name, email, phone, comments } = req.body;
+  // User the connection
+  connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
+
+    if (!err) {
+      // User the connection
+      connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+        // When done with the connection, release it
+        
+        if (!err) {
+          res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
+        } else {
+          console.log(err);
+        }
         console.log('The data from user table: \n', rows);
-         });
-       });
+      });
+    } else {
+      console.log(err);
     }
+    console.log('The data from user table: \n', rows);
+  });
+}
+
+// Delete User
+exports.delete = (req, res) => {
+
+  // Delete a record
+
+  // User the connection
+  // connection.query('DELETE FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+
+  //   if(!err) {
+  //     res.redirect('/');
+  //   } else {
+  //     console.log(err);
+  //   }
+  //   console.log('The data from user table: \n', rows);
+
+  // });
+
+  // Hide a record
+
+  connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
+    if (!err) {
+      let removedUser = encodeURIComponent('User successeflly removed.');
+      res.redirect('/?removed=' + removedUser);
+    } else {
+      console.log(err);
+    }
+    console.log('The data from beer table are: \n', rows);
+  });
+
+}
+
+// View Users
+exports.viewall = (req, res) => {
+
+  // User the connection
+  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+    if (!err) {
+      res.render('view-user', { rows });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+
+}
--- app.js
+++ app.js
@@ -1,50 +1,44 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const exphbs = require('express-handlebars'); // updated to 6.0.X
+//const bodyParser = require('body-parser');  // Remove
+//const mysql = require('mysql'); // Remove
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//  Middleware
+// Parsing middleware
 // Parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false })); // 
+app.use(express.urlencoded({extended: true})); // New
 
-//  application/json Parsing
+// Parse application/json
+// app.use(bodyParser.json());
+app.use(express.json()); // New
 
-app.use(bodyParser.json());                                        
-
-// Static-Files
+// Static Files
 app.use(express.static('public'));
 
+// Templating engine
+// app.engine('hbs', exphbs({ extname: '.hbs' })); // v5.3.4
+// app.set('view engine', 'hbs'); // v5.3.4
 
-// Handlebars-Template 
-app.engine('hbs', exphbs.engine( {extname: '.hbs'}));
-app.set('view engine', 'hbs');
+// Update to 6.0.X
+const handlebars = exphbs.create({ extname: '.hbs',});
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
 
 // Connection Pool
-const pool = mysql.createPool({
-     connectionLimit : 100,  
-     host            : process.env.DB_HOST,
-     user            : process.env.DB_USER,
-     password        : process.env.DB_PASS,
-     database        : process.env.DB_NAME
-});
+// You don't need the connection here as we have it in userController
+// let connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// });
+ 
+const routes = require('./server/routes/user');
+app.use('/', routes);
 
-// connect to db
-pool.getConnection((err, connection) => {
-    if (err) throw err; // broken connection
-    console.log('Connected as ID ' + connection.threadId);
-});
-
-
-
-const routes = require('./Server/routes/user');
-app.use ('/', routes);
-
-
-app.listen(port, () => console.log('listening on port ${port}'));
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
